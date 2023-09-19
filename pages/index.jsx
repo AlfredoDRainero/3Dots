a57119bf7fc326b7acf666a57faf6e9d8d3b0d02
Alfredo D Rainero <alfredo.rainero@gmail.com>
2023-09-19 20:06:37 -0300

    upgrade: name animation, squares responsive

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,31 +1,118 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
-import styled from 'styled-components';
+import styled, { keyframes } from "styled-components";
 
 const Container = styled.div`
   background-color: #dddddd;
   display: grid;
   grid-template-columns: repeat(8, 1fr);
   grid-template-rows: repeat(4, 1fr);
+  left:0;
+  top:0;
+  position:absolute;
   gap: 1px;
-  height: calc(100vh - 20px); /* Usar 100vh para ocupar el 100% de la altura de la ventana */
-  width: calc(100vw - 20px) ;//32px);
+  height: calc(100vh - 2px ); //- 20px ); /* Usar 100vh para ocupar el 100% de la altura de la ventana */
+  width: calc(100vw - 2px);// - 20px); //32px);
   //max-width: 1023px;
-  margin-right:25px;
+  // margin-right: 25px;
   @media (max-width: 1023px) {
     grid-template-columns: repeat(4, 1fr);
     grid-template-rows: repeat(4, 1fr);
   }
 `;
 
+const bounce = keyframes`
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-50%);
+  }
+`;
+
+const slideUp = keyframes`
+  0% {
+    transform: translateY(100%);
+    opacity: 1;
+  }
+  50% {
+    transform: translateY(0%);
+    opacity: 1;
+  }
+  100% {
+    transform: translateY(-120%);
+    opacity: 1;
+  }
+`;
+
+const slideDown = keyframes`
+  0% {
+    transform: translateY(-120%); /* Cambiado a valor negativo para iniciar desde arriba */
+    opacity: 1;
+  }
+  50% {
+    transform: translateY(0%);
+    opacity: 1;
+  }
+  100% {
+    transform: translateY(100%); /* Cambiado a valor positivo para moverse hacia abajo */
+    opacity: 1;
+  }
+`;
+
 const Div = styled.div`
-  //background-color: #dddddd;
-  //padding: 16px;
-  text-align: center;
-  border: 1px solid black;
-  width: 100%; /* Ocupar el 100% del ancho disponible */
-  height: 100%; /* Ocupar el 100% del alto disponible */
+  text-align: right;
+  border: 1px solid #888888;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  //white-space: nowrap;
+  color: #212932;
+`;
+
+
+// ------ NOMBRE ---------
+const Letra1 = styled.div`
+  width: 100%;
+  height: 100%;
+  font-family: "Century Gothic Bold", sans-serif; /* Fuente Century Gothic Bold */
+  font-size: 300px; /* Tamaño de fuente 72px */
+  font-weight: bold;
+  animation: ${bounce} 20s linear infinite; /* Aplicar la animación */
+  white-space: pre-wrap;
+  line-height: 300px;
+`;
+
+const Letra2 = styled.div`
+  width: 100%;
+  height: 100%;
+  font-family: "Century Gothic Bold", sans-serif; /* Fuente Century Gothic Bold */
+  font-size: 300px; /* Tamaño de fuente 72px */
+  font-weight: bold;
+  animation: ${slideUp} 10s linear infinite; /* Aplicar la animación */
+  white-space: pre-wrap;
+  line-height: 240px;
+`;
+
+const Letra3 = styled.div`
+  width: 100%;
+  height: 100%;
+  font-family: "Century Gothic Bold", sans-serif; /* Fuente Century Gothic Bold */
+  font-size: 200px; /* Tamaño de fuente 72px */
+  font-weight: bold;
+  animation: ${slideDown} 10s linear infinite; /* Aplicar la animación */
+  white-space: pre-wrap;
+`;
+
+const Letra4 = styled.div`
+  text-align: left;
+  width: 100%;
+  height: 100%;
+  font-family: "Century Gothic Bold", sans-serif; /* Fuente Century Gothic Bold */
+  font-size: 200px; /* Tamaño de fuente 72px */
+  font-weight: bold;
+  animation: ${slideDown} 15s linear infinite; /* Aplicar la animación */
 `;
 
 const HomePage = () => {
@@ -37,28 +124,48 @@ const HomePage = () => {
       setTotalDivs(isWideScreen ? 32 : 16);
     };
 
-    // Agregar un event listener para cambiar el totalDivs cuando se cambie el tamaño de la ventana
-    window.addEventListener('resize', handleResize);
-
-    // Llamar a handleResize al cargar la página inicialmente
+    window.addEventListener("resize", handleResize);
     handleResize();
-
-    // Limpiar el event listener cuando el componente se desmonte
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
     <div>
       <Container>
-        {[...Array(totalDivs)].map((_, index) => (
-          <Div key={index}>Div {index + 1}</Div>
-        ))}
+        {[...Array(totalDivs)].map((_, index) => {
+          let content;
+
+          switch (index) {
+            case 9:
+              content = <Letra1>2</Letra1>;
+              break;
+            case 10:
+              content = <Letra2>D</Letra2>;
+              break;
+            case 19:
+              content = <Letra3>E E E E E E E</Letra3>;
+              break;
+            case 20:
+              content = <Letra1>V V V</Letra1>;
+              break;
+            case 21:
+              content = <Letra3>S</Letra3>;
+              break;
+            case 30:
+              content = <Letra4>.</Letra4>;
+              break;
+            default:
+              content = "";
+              break;
+          }
+
+          return <Div key={index}>{content}</Div>;
+        })}
       </Container>
     </div>
   );
 };
-
 
 export default HomePage;
