c0fcfdb9a1ae6457e5b0b2fa5ef07a61c546238f
Alfredo D Rainero <alfredo.rainero@gmail.com>
2023-10-23 23:03:36 -0300

    Update: scroll 2 section

--- Sections/Section2.js
+++ Sections/Section2.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 const AppContainer = styled.div`
   display: flex;
@@ -7,10 +7,17 @@ const AppContainer = styled.div`
 `;
 
 const LeftComponent = styled.div`
+  padding-left: 100px;
   flex: 1;
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: #2f3b49ff;
+  color: white;
+  font-family: "Century Gothic", sans-serif; /* Establece Century Gothic como la fuente principal */
+  font-weight: normal; /* Establece el peso de la fuente en negrita (bold) */
+  font-size: 30px;
+  letter-spacing: 3px;
 `;
 
 const RightComponent = styled.div`
@@ -18,64 +25,196 @@ const RightComponent = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  overflow: hidden; /* Oculta el desbordamiento de las imágenes */
-  background-color: brown;
+  overflow: hidden;
+  //background-color: brown;
+  background-color: #2f3b49ff;
 `;
 
-const ImageContainer = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: gray;
-  img {
-    width: 100%;
-    height: 100%;
-  }
+const Div1 = styled.div`
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 2;
+  display: grid;
+  z-index: 1;
 `;
 
 const Section2 = () => {
-  const image = 'imagen.jpg';
-  const [scroll, setScroll] = useState(window.innerHeight - 200);
+  const images = [
+    "/screenworks/imagen1.jpg",
+    "/screenworks/imagen2.jpg",
+    "/screenworks/imagen3.jpg",
+    "/screenworks/imagen4.jpg",
+    "/screenworks/imagen5.jpg",
+    "/screenworks/imagen1.jpg",
+    "/screenworks/imagen2.jpg",
+  ];
+
+  const images2 = [
+    "/screenworks/imagen1.jpg",
+    "/screenworks/imagen2.jpg",
+    "/screenworks/imagen3.jpg",
+    "/screenworks/imagen4.jpg",
+    "/screenworks/imagen5.jpg",
+    "/screenworks/imagen7.jpg",
+    "/screenworks/imagen8.jpg",
+    "/screenworks/imagen1.jpg",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex2, setCurrentIndex2] = useState(0.5);
 
   useEffect(() => {
-    const imageHeight = (window.innerHeight / 2) + 200;
-    const scrollSpeed = 1; // Velocidad de desplazamiento
-
-    const animateScroll = () => {
-      setScroll(scroll => {
-        const newScroll = scroll - scrollSpeed;
-
-        // Cuando la imagen llega a la parte superior, reiniciamos su posición
-        if (newScroll <= -imageHeight) {
-          return window.innerHeight - 200; // Reinicia desde abajo
-        }
-
-        return newScroll;
-      });
-    };
-
-    const interval = setInterval(animateScroll, 5);
-
+    const nextIndex = (currentIndex + 1) % 5;
+    const interval = setInterval(() => {
+      setCurrentIndex(nextIndex);
+    }, 1200);
     return () => clearInterval(interval);
-  }, []);
+  }, [currentIndex, images]);
+
+  useEffect(() => {
+    const nextIndex2 = (currentIndex2 + 1) % 5;
+    const interval2 = setInterval(() => {
+      setCurrentIndex2(nextIndex2);
+    }, 1200);
+    return () => clearInterval(interval2);
+  }, [currentIndex2, images2]);
 
   return (
     <AppContainer>
       <LeftComponent>
-        <p>Texto centrado</p>
+        <p>
+          {" "}
+          We are a company that is passionate about developing cutting-edge
+          software to meet all of your digital needs.{" "}
+        </p>
       </LeftComponent>
-      <RightComponent>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <ImageContainer
-            style={{
-              transform: `translateY(${scroll}px)`,
-            }}
-          >
-            {image ? <img src={image} alt={`Imagen`} /> : <div style={{ background: 'gray' }}></div>}
-          </ImageContainer>
-        </div>
-      </RightComponent>
+      <Section1>
+        <Div1>
+          <RightComponent>
+            <CarouselContainer>
+              <Carousel
+                style={{ transform: `translateY(-${currentIndex * 33.33}%)` }}
+              >
+                {images.map((image, index) => (
+                  <CarouselItem key={index}>
+                    <Image src={image} alt={`Image ${index + 1}`} />
+                  </CarouselItem>
+                ))}
+              </Carousel>
+            </CarouselContainer>
+          <CarouselContainer3></CarouselContainer3>
+            <CarouselContainer>
+              <Carousel
+                style={{ transform: `translateY(-${currentIndex2 * 33.33}%)` }}
+              >
+                {images2.map((image2, index) => (
+                  <CarouselItem2 key={index}>
+                    <Image2 src={image2} alt={`Image ${index + 1}`} />
+                  </CarouselItem2>
+                ))}
+              </Carousel>
+            </CarouselContainer>
+          </RightComponent>
+        </Div1>
+        <Degrade />
+        <Degrade2 />
+      </Section1>
     </AppContainer>
   );
 };
+
+const Section1 = styled.div`
+
+flex: 2;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-template-rows: repeat(1, 1fr);
+
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 2;
+`;
+
+const Degrade = styled.div`
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 2;
+  display: grid;
+  z-index: 2;
+
+  width: 100%; /* Sin comillas */
+  height: 100vh; /* Sin comillas */
+  background: linear-gradient(
+    to bottom,
+    rgba(47, 59, 73, 0),
+    rgba(47, 59, 73, 0),
+    rgba(47, 59, 73, 1)
+  );
+`;
+
+const Degrade2 = styled.div`
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 2;
+  display: grid;
+  z-index: 2;
+
+  width: 100%; /* Sin comillas */
+  height: 100vh; /* Sin comillas */
+  background: linear-gradient(
+    to bottom,
+    rgba(47, 59, 73, 1),
+    rgba(47, 59, 73, 0),
+    rgba(47, 59, 73, 0)
+  );
+`;
+
+const CarouselContainer = styled.div`
+  width: 100%;
+  position: relative;
+  //background-color: blue;
+  transform: rotate(30deg);
+`;
+const CarouselContainer3 = styled.div`
+  width: 20%;
+  position: relative;
+  //background-color: blue;
+`;
+
+const Carousel = styled.div`
+  display: flex;
+  transition: transform 0.5s;
+  //background-color: red;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+`;
+
+const CarouselItem = styled.div`
+  flex: 0 0 33.33%;
+  //background-color: black;
+  height: 300px;
+  margin-left: auto;
+`;
+
+const Image = styled.img`
+  width: 300px;
+  height: 300px;
+`;
+
+const CarouselItem2 = styled.div`
+  flex: 0 0 33.33%;
+  // background-color: gray;
+  height: 300px;
+`;
+
+const Image2 = styled.img`
+  width: 300px;
+  height: 300px;
+`;
 
 export default Section2;
--- components/SVGComponent2.jsx
+++ components/SVGComponent2.jsx
@@ -2,12 +2,11 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { gsap, Power1 } from "gsap";
 
-
 const StyledSVG = styled.div`
- //background-color: red;
+  //background-color: red;
   /* Definir las variables para el ancho y el alto aquí */
-  --svg-width: 400px;
-  --svg-height: 100px;
+  --svg-width: 350px;
+  --svg-height: 150px;
   z-index: 3;
   svg {
     width: var(--svg-width);
@@ -16,74 +15,57 @@ const StyledSVG = styled.div`
 `;
 
 const SVGComponent2 = () => {
-
   return (
     <StyledSVG>
       <svg
-        width="90.77079"
-        height="21.204"
-        viewBox="0 0 90.77079 21.204001"
-        version="1.1"
+        width="610mm"
+        height="260mm"
+        viewBox="0 0 610 260"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g id="layer1" transform="translate(-0.6575461,-0.35936878)">
-          <g
-            id="g14"
-            transform="matrix(3.7795276,0,0,3.7795276,-565.61198,-201.03481)"
-          >
-            <g id="g1" transform="translate(-0.841899,0.93544333)">
-              <text
-                style={{
-                  
-                  fontStyle: "normal",
-                  fontVariant: "normal",
-                  fontWeight: "bold",
-                  fontStretch: "normal",
-                  fontSize: "7.73359px",
-                  fontFamily: "'Century Gothic'",
-                  inkscapeFontSpecification: "'Century Gothic Bold'",
-                  fill: "#dddddd",
-                  fillOpacity: 1,
-                  stroke: "none",
-                  strokeWidth: "0.241676",
-                }}
-                x="137.68492"
-                y="63.214417"
-                id="text7283-4-3-1-7-0-4-4-1-2"
-                transform="scale(1.0929721,0.91493644)"
-              >
-                <tspan
-                  id="tspan7281-5-0-1-8-4-2-5-9-43"
-                  x="137.68492"
-                  y="63.214417"
-                >
-                  3Dots
-                </tspan>
-              </text>
-              <text
-                style={{
-                  fontStyle: "normal",
-                  fontVariant: "normal",
-                  fontWeight: "bold",
-                  fontStretch: "normal",
-                  fontSize: "8.32995px",
-                  fontFamily: "Century Gothic, Arial, sans-serif",
-                  fill: "#e14c5f",
-                  fillOpacity: 1,
-                  stroke: "none",
-                  strokeWidth: "0.195233",
-                }}
-                x="165.41731"
-                y="53.509296"
-                id="text4-0-6"
-              >
-                <tspan id="tspan4-2-2" x="165.41731" y="53.509296">
-                  ...
-                </tspan>
-              </text>
-            </g>
-          </g>
-        </g>
+        <text
+          x="134.98218"
+          y="71.648537"
+          style={{
+            fontStyle: "normal",
+            fontVariant: "normal",
+            fontWeight: "bold",
+            fontStretch: "normal",
+            fontSize: "406.202px",
+            fontFamily: "Century Gothic",
+            fill: "#e14c5f",
+            fillOpacity: 1,
+            stroke: "none",
+            strokeWidth: "9.52031",
+          }}
+        >
+          <tspan x="134.98218" y="71.648537">
+            ...
+          </tspan>
+        </text>
+        <text
+          x="2.2637494"
+          y="269.10172"
+          style={{
+            fontStyle: "normal",
+            fontVariant: "normal",
+            fontWeight: "bold",
+            fontStretch: "normal",
+            //fontSize: "7.73359px",
+            fontFamily: "'Century Gothic'",
+            inkscapeFontSpecification: "'Century Gothic Bold'",
+            fontSize: "208.124px",
+            fontFamily: "arial",
+            fill: "#ffffff",
+            fillOpacity: 1,
+            stroke: "none",
+            strokeWidth: "6.50397",
+          }}
+        >
+          <tspan x="2.2637494" y="249.10172">
+            3Dots
+          </tspan>
+        </text>
       </svg>
     </StyledSVG>
   );
